4f4a9f1ca4142e4ffca9e42b49eb2d1f52469381
Taha Tesser <tessertaha@gmail.com>
2024-08-11 17:19:24 +0300

    Masking sample

---
+++ playground/samples/MaskingSample.swift
@@ -0,0 +1,26 @@
+//
+//  MaskingSample.swift
+//  playground
+//
+//  Created by Taha Tesser on 11.08.2024.
+//
+
+import SwiftUI
+
+struct MaskingSample: View {
+  var body: some View {
+    Image(.park)
+      .resizable()
+      .scaledToFit()
+      .ignoresSafeArea()
+      .mask {
+        Image(systemName: "apple.logo")
+          .font(.system(size: 200))
+      }
+      .shadow(radius: 10)
+  }
+}
+
+#Preview {
+  MaskingSample()
+}

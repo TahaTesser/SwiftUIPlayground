ca97bfb9f3e43071b8ab5dc09c2ac429ccb1874d
Taha Tesser <tessertaha@gmail.com>
2024-08-18 17:47:21 +0300

    Update list spacing sample for macOS

--- playground/samples/Lists/ListSectionSpacingSample.swift
+++ playground/samples/Lists/ListSectionSpacingSample.swift
@@ -9,56 +9,65 @@ import SwiftUI
 
 struct ListSectionSpacingSample: View {
     var body: some View {
-        TabView {
-            Tab {
-                List {
-                    Section {
-                        Text("A List Item")
-                        Text("A Second List Item")
-                        Text("A Third List Item")
+        #if os(macOS)
+            Text("List section spacing is not available on macOS")
+                .font(.title)
+                .padding()
+        #else
+            TabView {
+                Tab {
+                    List {
+                        Section {
+                            Text("A List Item")
+                            Text("A Second List Item")
+                            Text("A Third List Item")
+                        }
+                        Section {
+                            Text("A List Item")
+                            Text("A Second List Item")
+                            Text("A Third List Item")
+                        }
                     }
-                    Section {
-                        Text("A List Item")
-                        Text("A Second List Item")
-                        Text("A Third List Item")
+                    .listSectionSpacing(.default)
+                } label: {
+                    Text("Default")
+                }
+                Tab {
+                    List {
+                        Section {
+                            Text("A List Item")
+                            Text("A Second List Item")
+                            Text("A Third List Item")
+                        }
+                        Section {
+                            Text("A List Item")
+                            Text("A Second List Item")
+                            Text("A Third List Item")
+                        }
                     }
-                }.listSectionSpacing(.default)
-            } label: {
-                Text("Default")
+                    .listSectionSpacing(.compact)
+                } label: {
+                    Text("Compact")
+                }
+                Tab {
+                    List {
+                        Section {
+                            Text("A List Item")
+                            Text("A Second List Item")
+                            Text("A Third List Item")
+                        }
+                        Section {
+                            Text("A List Item")
+                            Text("A Second List Item")
+                            Text("A Third List Item")
+                        }
+                    }
+                    .listSectionSpacing(70)
+                } label: {
+                    Text("Custom")
+                }
             }
-            Tab {
-                List {
-                    Section {
-                        Text("A List Item")
-                        Text("A Second List Item")
-                        Text("A Third List Item")
-                    }
-                    Section {
-                        Text("A List Item")
-                        Text("A Second List Item")
-                        Text("A Third List Item")
-                    }
-                }.listSectionSpacing(.compact)
-            } label: {
-                Text("Compact")
-            }
-            Tab {
-                List {
-                    Section {
-                        Text("A List Item")
-                        Text("A Second List Item")
-                        Text("A Third List Item")
-                    }
-                    Section {
-                        Text("A List Item")
-                        Text("A Second List Item")
-                        Text("A Third List Item")
-                    }
-                }.listSectionSpacing(70)
-            } label: {
-                Text("Custom")
-            }
-        }
+        #endif
     }
 }
 
